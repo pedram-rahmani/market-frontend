de176b57c21f0378db89e9f0922b5e74e283c38b
Pedram Rahmani <pedramr.dev@gmail.com>
2026-09-14 23:47:49 +0330

    feat(api): make axios base url dynamic using environment variables

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://my-market-backend.liara.run/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
-    // if no tokens were in localstorage then check the session storage
     const token = localStorage.getItem("token") || sessionStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
