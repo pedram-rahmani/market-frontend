d852eaf5e7efcd644edee75676013fad540685ec
Pedram Rahmani <pedramr.dev@gmail.com>
2026-09-20 20:07:39 +0330

    update: update skeletons.tsx

--- components/ui/Skeletons/Skeletons.tsx
+++ components/ui/Skeletons/Skeletons.tsx
@@ -1,13 +1,11 @@
 import { HTMLAttributes } from "react";
 
-interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {
-
-}
+interface SkeletonProps extends HTMLAttributes<HTMLDivElement> {}
 
 export function BaseSkeleton({ className = "w-full h-4", ...props }: SkeletonProps) {
   return (
     <div 
-      className={`bg-white/70 dark:bg-custom-gray-400/10 animate-pulse rounded-lg ${className}`} 
+      className={`bg-gray-200 dark:bg-custom-gray-400/10 animate-pulse rounded-lg ${className}`} 
       {...props}
     />
   );
@@ -15,7 +13,7 @@ export function BaseSkeleton({ className = "w-full h-4", ...props }: SkeletonPro
 
 export function SkeletonCard() {
   return (
-    <div className="bg-white/70 dark:bg-custom-gray-400/10 p-4 rounded-xl border border-custom-gray-400 dark:border-custom-gray-400/20 text-center flex flex-col items-center justify-center gap-y-2 animate-pulse">
+    <div className="bg-white dark:bg-custom-gray-400/10 p-4 rounded-xl border border-gray-200 dark:border-custom-gray-400/20 text-center flex flex-col items-center justify-center gap-y-2 animate-pulse shadow-sm">
       <BaseSkeleton className="w-20 h-4" />
       <BaseSkeleton className="w-12 h-6" />
     </div>
